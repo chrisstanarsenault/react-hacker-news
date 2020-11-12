bf69becfb7d47154187574df99ed90263616dd88
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-11-12 03:52:21 -0500

    added basic header styling for multiple media queries

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
   }, []);
 
   return (
-    <main className="box-border">
+    <main>
       <Header />
       {Object.keys(currentStories.length !== 0) && (
         <AllStoriesContainer stories={currentStories} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-orange-500 flex flex-col items-center p-2 w-screen">
+    <header className="bg-orange-500 flex flex-col lg:flex-row lg:justify-around items-center p-2 w-screen">
       <h1 className="text-2xl text-center font-bold mb-1">HackerNews Feed</h1>
-      <form className="flex justify-between w-full">
+      <form className="flex justify-center lg:justify-end py-2 w-full lg:w-1/5">
         <input
           name="search-bar"
           id="search-bar"
           placeholder="Search for a story"
-          className="rounded px-2 py-1 w-full mr-4"
+          className="rounded border border-black px-2 py-1 w-3/4 lg:w-full relative lg:left-7"
         />
-        <button>
+        <button className="relative left-1 xs:left-2 lg:left-9">
           <i className="fas fa-search fa-lg"></i>
         </button>
       </form>
