4a7d30b8c6ff166c4f95b99741d82fefb67f3926
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-11-15 05:34:12 -0500

    added basic functionality for infinite scroll.  Now need to have to stop running once max page has been hit

--- src/components/AllStoriesContainer.js
+++ src/components/AllStoriesContainer.js
@@ -3,14 +3,13 @@ import React from "react";
 import Story from "./Story";
 
 export default function AllStoriesContainer(props) {
-  const { stories } = props;
+  const { stories, testRef } = props;
 
   return (
     <section className="flex flex-col">
       {Object.keys(stories).length !== 0 &&
-        stories.hits.map((story) => (
-          <Story key={story.objectID} story={story} />
-        ))}
+        stories.map((story) => <Story key={story.objectID} story={story} />)}
+      <div className="border-t-2 border-red-400" ref={testRef}></div>
     </section>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,89 @@
-import React, { useState, useEffect } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useRef,
+  useCallback,
+} from "react";
 
 import AllStoriesContainer from "./AllStoriesContainer";
 import Header from "./Header";
 
 function App() {
-  const [currentStories, setCurrentStories] = useState({});
+  const [currentStories, setCurrentStories] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [pageNum, setPageNum] = useState(0);
+  const [maxPages, setMaxPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const storyReducer = (state, action) => {};
+
+  const pageReducer = (state, action) => {
+    switch (action.type) {
+      case "ADVANCE_PAGE":
+        console.log(state.page);
+        return { ...state, page: state.page + 1 };
+      default:
+        return state;
+    }
+  };
+
+  const [pager, pagerDispatch] = useReducer(pageReducer, { page: 0 });
 
   const searchStoryFromInput = async (e, input) => {
     e.preventDefault();
-    setCurrentStories({});
+    setCurrentStories([]);
     setIsLoading(true);
     await fetch(
-      `http://hn.algolia.com/api/v1/search?query=${input}&page=${pageNum}`
+      `http://hn.algolia.com/api/v1/search?query=${input}&page=${pager.page}`
     )
       .then((res) => res.json())
       .then((data) => {
-        setCurrentStories(data);
+        setCurrentStories([...currentStories, ...data.hits]);
+        setMaxPages(data.nbPages - 1);
       });
     setTimeout(() => {
       setIsLoading(false);
     }, 1500);
   };
 
+  let bottomBoundaryRef = useRef(null);
+  const scrollObserver = useCallback(
+    (node) => {
+      new IntersectionObserver((entries) => {
+        entries.forEach((en) => {
+          console.log(pager.page);
+          if (en.intersectionRatio > 0 && pager.page < maxPages) {
+            console.log("load more");
+            pagerDispatch({ type: "ADVANCE_PAGE" });
+          }
+        });
+      }).observe(node);
+    },
+    [pagerDispatch, maxPages]
+  );
+
   useEffect(() => {
-    const getFrontPageData = async () => {
+    const getFrontPageData = async (page) => {
       await fetch(
-        `http://hn.algolia.com/api/v1/search?tags=front_page&page=${pageNum}`
+        `http://hn.algolia.com/api/v1/search?tags=front_page&page=${page}`
       )
         .then((res) => res.json())
         .then((data) => {
-          console.log(data.hits);
+          setMaxPages(data.nbPages - 1);
           setIsLoading(false);
-          setCurrentStories(data);
+          setCurrentStories([...currentStories, ...data.hits]);
         });
     };
     setTimeout(() => {
-      getFrontPageData();
+      getFrontPageData(pager.page);
     }, 1500);
-  }, []);
+  }, [pager.page]);
+
+  useEffect(() => {
+    if (bottomBoundaryRef.current) {
+      scrollObserver(bottomBoundaryRef.current);
+    }
+  }, [scrollObserver, bottomBoundaryRef, isLoading]);
 
   return (
     <main className="bg-yellow-100 w-screen h-full relative">
@@ -50,7 +94,10 @@ function App() {
         </h1>
       )}
       {Object.keys(currentStories.length !== 0) && !isLoading && (
-        <AllStoriesContainer stories={currentStories} />
+        <AllStoriesContainer
+          stories={currentStories}
+          testRef={bottomBoundaryRef}
+        />
       )}
     </main>
   );
